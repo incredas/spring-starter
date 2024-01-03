7bb975fc9a7c75d8c506c0af9367fb1d6775caff
Vadym Doroshevych <vd@incredas.dev>
2024-01-03 19:32:38 +0100

    - Added javadoc for CrudController.java;

--- lib/src/main/java/dev/incredas/spring/starter/web/CrudController.java
+++ lib/src/main/java/dev/incredas/spring/starter/web/CrudController.java
@@ -8,8 +8,22 @@ import org.springdoc.core.converters.models.PageableAsQueryParam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * The CrudController class is a generic controller class that handles CRUD operations for a specific resource.
+ *
+ * @param <ID>      the type of the resource identifier
+ * @param <REQUEST> the type of the HTTP request body for create and update operations
+ * @param <QUERY>   the type of the HTTP request parameters for filtering and sorting operations
+ * @param <RESPONSE> the type of the HTTP response body
+ */
 @RequiredArgsConstructor
 @SuppressWarnings("squid:S119")
 public class CrudController<ID, REQUEST, QUERY, RESPONSE> {
