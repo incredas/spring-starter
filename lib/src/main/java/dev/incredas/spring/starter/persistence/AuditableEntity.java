d596bcea8dba5f4c66cbd329e28c1d719bee7ee0
Vadym Doroshevych <vd@incredas.dev>
2024-01-03 19:31:13 +0100

    - Added javadoc for AuditableEntity.java;

--- lib/src/main/java/dev/incredas/spring/starter/persistence/AuditableEntity.java
+++ lib/src/main/java/dev/incredas/spring/starter/persistence/AuditableEntity.java
@@ -1,12 +1,34 @@
 package dev.incredas.spring.starter.persistence;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.beans.Transient;
 import java.io.Serializable;
 import java.time.ZonedDateTime;
 
+/**
+ * The AuditableEntity class is an abstract class that represents an entity with audit fields.
+ *
+ * <p>
+ * The audit fields include createdAt, createdBy, modifiedBy, and modifiedAt.
+ * These fields are automatically populated and updated through the @PrePersist and @PreUpdate annotations.
+ * </p>
+ *
+ * <p>
+ * This class is meant to be extended by other entity classes, such as ENTITY, in order to inherit
+ * the audit fields from AuditableEntity.
+ * </p>
+ *
+ * <p>
+ * Note that the getCreatorUsername method is abstract and must be implemented by the subclasses in
+ * order to provide the username of the creator user.
+ * </p>
+ */
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AuditableEntity implements Serializable {
