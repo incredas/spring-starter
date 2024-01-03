a34683db851c500f8ea78ad17875de5177623d38
Vadym Doroshevych <vd@incredas.dev>
2024-01-03 19:49:11 +0100

    - Added javadoc for interface and methods in HttpServiceException.java;

--- lib/src/main/java/dev/incredas/spring/starter/exception/HttpServiceException.java
+++ lib/src/main/java/dev/incredas/spring/starter/exception/HttpServiceException.java
@@ -5,6 +5,19 @@ import lombok.Getter;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * The HttpServiceException class is an abstract base class for all HTTP service-related exceptions.
+ *
+ * <p>
+ * This class extends the RuntimeException class and provides common properties and behavior for HTTP service exceptions.
+ * It defines the status code, message, and set of errors associated with the exception.
+ * </p>
+ *
+ * <p>
+ * The HttpServiceException class cannot be directly instantiated. Instead, it should be extended by concrete exception
+ * classes for specific types of HTTP service errors.
+ * </p>
+ */
 @Getter
 public abstract class HttpServiceException extends RuntimeException {
 
